e156f9d91123de96728d6a075228bc26f8b08342
Wagura Maurice <wagura465@gmail.com>
2021-07-01 13:03:31 +0300

    welcome page returned

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return redirect(route('login'));
+    return view('welcome');
 });
 
 // Lipa na Mpesa Online Transactions
