705db80481c90adc16f7d4dc4dfb073829582865
Wagura Maurice <wagura465@gmail.com>
2021-07-05 13:01:59 +0300

    assets re-building

--- public/js/app.js
+++ public/js/app.js
@@ -21338,8 +21338,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /var/www/html/bebing/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /var/www/html/bebing/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\laragon\www\rware\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\laragon\www\rware\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
